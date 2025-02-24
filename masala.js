3406515608d8e3f8dd2c68a065ae5c1ea98c3bf6
AtamjonRaximov <raximovatam999@gmail.com>
2025-02-24 08:13:58 +0500

    add masala js

---
+++ masala.js
@@ -0,0 +1,95 @@
+let uzb = {
+    maydoni : 448900,
+    axolisi : 35650000,
+    poytaxti : "Toshkent",
+    viloyatlari : {
+        "Andijon": {
+            shaharlari: ["Asaka", "Andijon", "Shahrixon" ],
+            maydon : 4027,
+            markazi : "Andijon",
+            axolisi : 3420000,
+        },
+        "Farg'ona": {
+            shaharlari: ["Qo'qon", "Farg'ona", "Dang'ara"],
+            maydon : 6800,
+            markazi : "Farg'ona",
+            axolisi : 4123000,
+        },
+        "Namangan" : {
+            shaharlari : ["Chust", "Namangan", "Kosonsoy"],
+            maydon : 7900,
+            markazi : "Namangan",
+            axolisi : 3100000,
+        },
+        "Toshkent": {
+            shaharlari : ["Zangiota", "Toshkent", "Qibray"],
+            maydon : 15300,
+            markazi : "Toshkent",
+            axolisi : 6150000,
+        },
+        "Sirdaryo": {
+            shaharlari : ["Yangiyer", "Gulistin", "Sirdaryo"],
+            maydon : 4028,
+            markazi : "Guliston",
+            axolisi : 914000,
+        },
+        "Jizzax": {
+            shaharlari : ["Do'stlik", "Jizzah", "Zomin"],
+            maydon : 21200,
+            markazi : "Jizzax",
+            axolisi : 1500000,
+        },
+        "Samarqand": {
+            shaharlari : ["Ishtixon", "Samarqand", "Jomboy"],
+            maydon : 16400,
+            markazi : "Samarqand",
+            axolisi : 4159000,
+        },
+        "Qashqadaryo": { 
+            shaharlari : ["Shahrisabiz", "Qarshi", "Kitob"], 
+            maydon : 28500,
+            markazi : "Qarshi",
+            axolisi : 3500000,
+        },
+        "Surxandaryo": {
+            shaharlari: ["Denov", "Termiz", "Boysun"],
+            maydon : 20000,
+            markazi : "Termiz",
+            axolisi : 3000000,
+        },
+        "Buxoro": {
+            shaharlari: ["G'ishdivon", "Buxoro", "Vobkent"],
+            maydon : 40000,
+            markazi : "Buxoro",
+            axolisi : 2000000,
+        },
+        "Navoiy": {
+            shaharlari : ["Zarafshon", "Navoiy", "Uchquduq"],
+            maydon : 111100,
+            markazi : "Navoiy",
+            axolisi : 1075000,
+        },
+        "Xorazim": {
+            shaharlari : ["Xiva", "Urganch", "Xazorasp"],
+            maydon : 6300,
+            markazi : "Urganch",
+            axolisi : 2000000,
+        },
+
+        "Qoraqolpog'iston" : {
+            shaharlari : ["Xo'jali", "Nukus", "Qo'ng'irot"], 
+            maydon : 166000,
+            markazi : "Nukus",
+            axolisi : 2000000,
+        }
+    },
+    telefonKodi : "+998"
+}
+
+
+// 1) Prompt yordamida berilgan so'rovga tegishli malumotlarni chiqarib beramiz
+// Nukus deb yozilsa => Qoraqolpog'iston
+// Qoraqolpog'iston deb yozilsa => maydoni, aholisi, markazi
+// 2) Urganch deb yozilsa axolisi yoki maydoni boyicha nechinchi o'rindaligini
+// chiqarishimiz kerak boladi
+// 3) telefonKodi yozilsa uzbekistan telefon kodi chiqishi kerak
